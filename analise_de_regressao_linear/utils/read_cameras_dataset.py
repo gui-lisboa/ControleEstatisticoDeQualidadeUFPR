aa49d6f3080e9bf2476267b7302214bfa60dde46
Guilherme Lisboa <g.lisboa.oliveira@outlook.com>
2021-10-24 20:22:06 -0300

    adiciona atividade 3 de regressão linear

--- analise_de_regressao_linear/utils/read_cameras_dataset.py
+++ analise_de_regressao_linear/utils/read_cameras_dataset.py
@@ -1,6 +1,8 @@
 import pandas
+import warnings
 
 def read_cameras_dataset():
+    warnings.filterwarnings('ignore')
     colunas_data = ['last_update', 'release_date', 'release_month']
     dataset = pandas.read_table("http://leg.ufpr.br/~walmes/data/digital-cameras.txt", comment="#", parse_dates=colunas_data)
     dataset = dataset.convert_dtypes()
