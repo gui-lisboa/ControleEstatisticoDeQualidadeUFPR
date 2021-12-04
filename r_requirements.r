030e7ac44ccbc00ce323304e712ff74922f73404
Guilherme Lisboa <g.lisboa.oliveira@outlook.com>
2021-12-04 17:01:02 -0300

    adiciona desafio 2 de viz de dados

--- r_requirements.r
+++ r_requirements.r
@@ -1,3 +1,8 @@
 # Instala IRKenel para Jupyter Notebooks
 install.packages("IRkernel")
 IRkernel::installspec()
+
+install.packages("ggplot2")
+install.packages("ggthemes")
+install.packages("plotly")
+install.packages("dplyr")
