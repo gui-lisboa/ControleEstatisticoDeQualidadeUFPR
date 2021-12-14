1d5d5a8a015244070697fb6fc28a02853f7e3622
Guilherme Lisboa <g.lisboa.oliveira@outlook.com>
2021-12-14 15:38:43 -0300

    adiciona avaliacao2 de viz de dados

--- r_requirements.r
+++ r_requirements.r
@@ -2,6 +2,9 @@
 install.packages("IRkernel")
 IRkernel::installspec()
 
+install.packages("remotes")
 install.packages("ggplot2")
 install.packages("ggthemes")
 install.packages("dplyr")
+
+remotes::install_github("davidsjoberg/ggsankey")
