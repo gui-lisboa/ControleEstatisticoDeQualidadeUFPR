534e62b9b52c0e7becbb160e4347eff3b209cec9
Guilherme Lisboa <g.lisboa.oliveira@outlook.com>
2022-03-11 10:03:39 -0300

    corrige warnings e erros no build

--- r_requirements.r
+++ r_requirements.r
@@ -2,11 +2,6 @@
 install.packages("IRkernel")
 IRkernel::installspec()
 
-install.packages("curl")
-install.packages("remotes")
 install.packages("ggplot2")
 install.packages("ggthemes")
 install.packages("dplyr")
-
-install.packages("devtools")
-devtools::install_github("davidsjoberg/ggsankey")
